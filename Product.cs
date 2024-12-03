2f12177a3636a933223829d948b924dc70562d91
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-12-03 12:33:22 -0600

    Add fields to Product class

--- Product.cs
+++ Product.cs
@@ -2,4 +2,12 @@ public class Product
 {
     public int ProductId { get; set; }
     public string ProductName { get; set; }
+    public int SupplierId { get; set; }
+    public int CategoryId { get; set; }
+    public int QuantityPerUnit { get; set; }
+    public decimal UnitPrice { get; set; }
+    public int UnitsInStock { get; set; }
+    public int UnitsOnOrder { get; set; }
+    public int ReorderLevel { get; set; }
+    public bool Discontinued { get; set; }
 }
