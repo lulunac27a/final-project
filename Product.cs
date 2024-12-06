02f7a2a172dc2d243b860c1a960bd80594354f3a
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-12-06 17:22:23 -0600

    Add method to edit products

--- Product.cs
+++ Product.cs
@@ -1,7 +1,7 @@
 public class Product
 {
     public int ProductId { get; set; }
-    public string ProductName { get; set; }
+    public string? ProductName { get; set; }
     public int SupplierId { get; set; }
     public int CategoryId { get; set; }
     public int QuantityPerUnit { get; set; }
--- Program.cs
+++ Program.cs
@@ -10,16 +10,38 @@ do
 {
     Console.WriteLine("Enter your choice:");
     Console.WriteLine("1. Add product");
+    Console.WriteLine("2. Edit product");
     string? choice = Console.ReadLine();
+    var db = new DataContext();
     switch (choice)
     {
         case "1":
-            var db = new DataContext();
+
             Product? product = InputProduct(db, logger);
             db.AddProduct(product);
             logger.Info("Product added successfully");
             break;
-
+        case "2":
+            var products = db.Products.OrderBy(p => p.ProductId);
+            foreach (Product p in products)
+            {
+                Console.WriteLine($"{p.ProductId}. {p.ProductName}");
+            }
+            Product? foundProduct = null;
+            if (int.TryParse(Console.ReadLine(), out var ProductId))
+            {
+                foundProduct = db.Products.Find(ProductId);
+            }
+            if (foundProduct != null)
+            {
+                Product UpdatedProduct = InputProduct(db, logger);
+                UpdatedProduct.ProductId = foundProduct.ProductId;
+                string productName = Console.ReadLine();
+                UpdatedProduct.ProductName = productName;
+                db.EditProduct(UpdatedProduct);
+                logger.Info("Product edited successfully");
+            }
+            break;
         default:
             logger.Info("Program ended");
             break;
