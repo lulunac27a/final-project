332a7b676a41b0bd5b5140405f4e0dceeacfb2b2
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-12-06 19:54:14 -0600

    Update product details

--- Program.cs
+++ Program.cs
@@ -79,7 +79,7 @@ do
             }
             if (foundAProduct != null)
             {
-                Console.WriteLine($"ID: {foundAProduct.ProductId}, Name: {foundAProduct.ProductName}, Supplier ID: {foundAProduct.SupplierId}, Category ID: {foundAProduct.CategoryId}, Quantity Per Unit: {foundAProduct.QuantityPerUnit}, Unit Price: {foundAProduct.UnitPrice}, Units On Order: {foundAProduct.UnitsOnOrder}, Reorder Level: {foundAProduct.ReorderLevel}, Discontinued: {foundAProduct.Discontinued}")
+                Console.WriteLine($"Product ID: {foundAProduct.ProductId}, Product Name: {foundAProduct.ProductName}, Supplier ID: {foundAProduct.SupplierId}, Category ID: {foundAProduct.CategoryId}, Quantity Per Unit: {foundAProduct.QuantityPerUnit}, Unit Price: {foundAProduct.UnitPrice}, Units On Order: {foundAProduct.UnitsOnOrder}, Reorder Level: {foundAProduct.ReorderLevel}, Discontinued: {foundAProduct.Discontinued}")
             }
 
             break;
