37c2cb403acb02602735255ece85c95e4726fea9
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-12-08 17:23:41 -0600

    Add logging

--- DataContext.cs
+++ DataContext.cs
@@ -1,14 +1,48 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using NLog;
 
 public class DataContext : DbContext
 {
   public DbSet<Product> Products { get; set; }
 
+  private string path;
+  private Logger logger;
+
+  public DataContext()
+  {
+    path = Directory.GetCurrentDirectory() + "//nlog.config";
+    var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
+  }
 
   public void AddProduct(Product product)
   {
-    this.Products.Add(product);
+    if (product == null)
+    {
+      logger.Error("Product is null");
+      return;
+    }
+    Console.WriteLine("Enter category ID:");
+    Console.WriteLine("1. Beverages");
+    Console.WriteLine("2. Condiments");
+    Console.WriteLine("3. Confections");
+    Console.WriteLine("4. Dairy Products");
+    Console.WriteLine("5. Grains");
+    Console.WriteLine("6. Meat/Poultry");
+    Console.WriteLine("7. Produce");
+    Console.WriteLine("8. Seafood");
+    int categoryID = Convert.ToInt32(Console.ReadLine());
+    product.CategoryId = categoryID;
+    if (categoryID < 1 || categoryID > 8)
+    {
+      logger.Error("Invalid category ID");
+      return;
+    }
+    else
+    {
+      this.Products.Add(product);
+      logger.Info("Product added successfully");
+    }
     this.SaveChanges();
   }
   public void DeleteProduct(Product product)
--- Program.cs
+++ Program.cs
@@ -1,110 +1,136 @@
 ﻿using NLog;
-string path = Directory.GetCurrentDirectory() + "//nlog.config";
-
-// create instance of Logger
-var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
-
-logger.Info("Program started");
-
-do
+partial class Program
 {
-    Console.WriteLine("Enter your choice:");
-    Console.WriteLine("1. Add product");
-    Console.WriteLine("2. Edit product");
-    Console.WriteLine("3. Display Products");
-    Console.WriteLine("4. Display a Specific Product");
-    string? choice = Console.ReadLine();
-    var db = new DataContext();
-    switch (choice)
+    static void Main(string[] args)
     {
-        case "1":
 
-            Product? product = InputProduct(db, logger);
-            db.AddProduct(product);
-            logger.Info("Product added successfully");
-            break;
-        case "2":
-            var products = db.Products.OrderBy(p => p.ProductId);
-            foreach (Product p in products)
-            {
-                Console.WriteLine($"{p.ProductId}. {p.ProductName}");
-            }
-            Product? foundProduct = null;
-            if (int.TryParse(Console.ReadLine(), out var ProductId))
-            {
-                foundProduct = db.Products.Find(ProductId);
-            }
-            if (foundProduct != null)
-            {
-                Product UpdatedProduct = InputProduct(db, logger);
-                UpdatedProduct.ProductId = foundProduct.ProductId;
-                string productName = Console.ReadLine();
-                UpdatedProduct.ProductName = productName;
-                db.EditProduct(UpdatedProduct);
-                logger.Info("Product edited successfully");
-            }
-            else
-            {
-                logger.Error("Product not found");
-            }
-            break;
-        case "3":
-            Console.WriteLine("1. Display Discontinued Products");
-            Console.WriteLine("2. Display Active Products");
-            Console.WriteLine("3. Display All Products");
-            string discontinued = Console.ReadLine();
-            switch (discontinued)
+        string path = Directory.GetCurrentDirectory() + "//nlog.config";
+
+        // create instance of Logger
+        var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
+
+        logger.Info("Program started");
+
+        do
+        {
+            Console.WriteLine("Enter your choice:");
+            Console.WriteLine("1. Add product");
+            Console.WriteLine("2. Edit product");
+            Console.WriteLine("3. Display Products");
+            Console.WriteLine("4. Display a Specific Product");
+            string? choice = Console.ReadLine();
+            var db = new DataContext();
+            switch (choice)
             {
                 case "1":
-                    foreach (Product currentProduct in db.Products)
+
+                    Product? product = InputProduct(db, logger);
+                    if (product != null)
+                        try
+                        {
+                            db.AddProduct(product);
+                            logger.Info("Product added successfully");
+                        }
+                        catch (Exception ex)
+                        {
+                            logger.Error(ex, "Error adding product");
+                        }
+                    else
                     {
-                        if (currentProduct.Discontinued) Console.WriteLine(currentProduct.ProductName);
+                        logger.Error("Product is null");
                     }
+                    logger.Info("Product added successfully");
                     break;
                 case "2":
-                    foreach (Product currentProduct in db.Products)
+                    var products = db.Products.OrderBy(p => p.ProductId).ToList();
+                    if (products != null)
                     {
-                        if (!currentProduct.Discontinued) Console.WriteLine(currentProduct.ProductName);
+                        foreach (Product p in products)
+                        {
+                            Console.WriteLine($"{p.ProductId}. {p.ProductName}");
+                        }
+                    }
+                    Product? foundProduct = null;
+                    if (int.TryParse(Console.ReadLine(), out var ProductId))
+                    {
+                        foundProduct = db.Products.Find(ProductId);
+                    }
+                    if (foundProduct != null)
+                    {
+                        Product? UpdatedProduct = InputProduct(db, logger);
+                        if (UpdatedProduct != null)
+                        {
+                            UpdatedProduct.ProductId = foundProduct.ProductId;
+                            string? productName = Console.ReadLine();
+                            UpdatedProduct.ProductName = productName;
+                            db.EditProduct(UpdatedProduct);
+                            logger.Info("Product edited successfully");
+                        }
+                    }
+                    else
+                    {
+                        logger.Error("Product not found");
                     }
                     break;
                 case "3":
-
-                    foreach (Product currentProduct in db.Products)
+                    Console.WriteLine("1. Display Discontinued Products");
+                    Console.WriteLine("2. Display Active Products");
+                    Console.WriteLine("3. Display All Products");
+                    string discontinued = Console.ReadLine();
+                    switch (discontinued)
                     {
-                        Console.WriteLine(currentProduct.ProductName);
+                        case "1":
+                            foreach (Product currentProduct in db.Products)
+                            {
+                                if (currentProduct.Discontinued) Console.WriteLine(currentProduct.ProductName);
+                            }
+                            break;
+                        case "2":
+                            foreach (Product currentProduct in db.Products)
+                            {
+                                if (!currentProduct.Discontinued) Console.WriteLine(currentProduct.ProductName);
+                            }
+                            break;
+                        case "3":
+
+                            foreach (Product currentProduct in db.Products)
+                            {
+                                Console.WriteLine(currentProduct.ProductName);
+                            }
+                            break;
+                        default:
+                            logger.Error("Invalid choice");
+                            break;
+                    }
+                    break;
+                case "4":
+                    Product? foundAProduct = null;
+                    if (int.TryParse(Console.ReadLine(), out var enteredProductId))
+                    {
+                        foundAProduct = db.Products.Find(enteredProductId);
+                    }
+                    if (foundAProduct != null)
+                    {
+                        logger.Info("Product found");
+                        Console.WriteLine($"Product ID: {foundAProduct.ProductId}, Product Name: {foundAProduct.ProductName}, Supplier ID: {foundAProduct.SupplierId}, Category ID: {foundAProduct.CategoryId}, Quantity Per Unit: {foundAProduct.QuantityPerUnit}, Unit Price: {foundAProduct.UnitPrice}, Units In Stock: {foundAProduct.UnitsInStock}, Units On Order: {foundAProduct.UnitsOnOrder}, Reorder Level: {foundAProduct.ReorderLevel}, Discontinued: {foundAProduct.Discontinued}");
+                    }
+                    else
+                    {
+                        logger.Error("Product not found");
                     }
                     break;
                 default:
-                    logger.Error("Invalid choice");
+                    logger.Info("Invalid choice. Program ended");
                     break;
             }
-            break;
-        case "4":
-            Product? foundAProduct = null;
-            if (int.TryParse(Console.ReadLine(), out var enteredProductId))
-            {
-                foundAProduct = db.Products.Find(enteredProductId);
-            }
-            if (foundAProduct != null)
-            {
-                logger.Info("Product found");
-                Console.WriteLine($"Product ID: {foundAProduct.ProductId}, Product Name: {foundAProduct.ProductName}, Supplier ID: {foundAProduct.SupplierId}, Category ID: {foundAProduct.CategoryId}, Quantity Per Unit: {foundAProduct.QuantityPerUnit}, Unit Price: {foundAProduct.UnitPrice}, Units In Stock: {foundAProduct.UnitsInStock}, Units On Order: {foundAProduct.UnitsOnOrder}, Reorder Level: {foundAProduct.ReorderLevel}, Discontinued: {foundAProduct.Discontinued}");
-            }
-            else
-            {
-                logger.Error("Product not found");
-            }
-            break;
-        default:
-            logger.Info("Invalid choice. Program ended");
-            break;
-    }
-} while (true);
-static Product? InputProduct(DataContext db, NLog.Logger logger)
-{
-    Product product = new();
-    Console.WriteLine("Enter product name:");
-    product.ProductName = Console.ReadLine();
-    return product;
-}
+        } while (true);
+        static Product? InputProduct(DataContext db, NLog.Logger logger)
+        {
+            Product product = new();
+            Console.WriteLine("Enter product name:");
+            product.ProductName = Console.ReadLine();
+            return product;
+        }
 
+    }
+}
